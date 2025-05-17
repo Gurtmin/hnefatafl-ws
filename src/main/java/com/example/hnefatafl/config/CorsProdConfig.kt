01b20f0d70f9f2465155169fbd09bd6f2b951d08
Petr Cermak <pcermak@multima.cz>
2025-05-17 22:57:22 +0200

    cors added

--- src/main/java/com/example/hnefatafl/config/CorsProdConfig.kt
+++ src/main/java/com/example/hnefatafl/config/CorsProdConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@Profile("prod")
+//@Profile("prod")
 class CorsProdConfig : BaseLoggable(), WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
